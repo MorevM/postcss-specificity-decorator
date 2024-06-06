efe88347b3862f1bb74e89947fb033e6053c6f78
MorevM <max.seainside@gmail.com>
2024-06-06 17:09:43 +0300

    chore: Bump deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,12 @@ module.exports = {
 	root: true,
 	extends: [
 		'@morev/eslint-config/preset/common',
+		'@morev/eslint-config/preset/vitest',
 	],
 	rules: {},
+	overrides: [
+		{
+			files: ['*.cjs'], extends: ['@morev/eslint-config/node'],
+		},
+	],
 };
